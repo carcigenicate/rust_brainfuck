2e535c55f8d1bae5ea8288bec14d22f9cd208684
carcigenicate <brendonw5@gmail.com>
2024-11-29 13:56:21 -0700

    Factored out the interpretation dispatch match into its own function, then wrote some tests that test individual instructions using it.

--- src/ezfuck/interpreter/interpreter.rs
+++ src/ezfuck/interpreter/interpreter.rs
@@ -73,56 +73,59 @@ fn read_value<R: BufRead>(in_stream: &mut R) -> u8 {
     return input[0];
 }
 
+pub fn interpret_instruction<R: BufRead, W: Write>(instruction: Instruction, state: &mut ExecutionState, in_stream: &mut R, out_stream: &mut W) -> () {
+    match instruction {
+        Instruction::ApplyOperatorToCell { operator, value } => {
+            let actual_value = value.determine_value(state.get_current_cell());
+            let new_cell_value = apply_math_operator(state.get_current_cell(), operator, actual_value);
+            state.set_current_cell(new_cell_value);
+        }
+
+        Instruction::AddToCellPtr { direction, offset } => {
+            let abs_offset = offset.determine_value(state.get_current_cell());
+            let signed_offset = if direction == Direction::Left { abs_offset as isize * -1 } else { abs_offset as isize };
+            let new_cell_ptr = add_cell_ptr_value(state.cell_ptr, signed_offset);
+            state.set_cell_pointer(new_cell_ptr);
+        }
+
+        Instruction::JumpToIf { position, operator, match_value } => {
+            match operator {
+                EqualityOperator::Equal => {
+                    if state.get_current_cell() == match_value {
+                        state.set_instruction_pointer(position);
+                    }
+                },
+                EqualityOperator::NotEqual => {
+                    if state.get_current_cell() != match_value {
+                        state.set_instruction_pointer(position);
+                    }
+                }
+            }
+        }
+
+        Instruction::PrintOut => {
+            print_value(out_stream, state.get_current_cell());
+        }
+
+        Instruction::ReadIn => {
+            let input = read_value(in_stream);
+            state.set_current_cell(input);
+        }
+
+        Instruction::SetCell { value } => {
+            let actual_value = value.determine_value(state.get_current_cell());
+            state.set_current_cell(actual_value);
+        }
+    }
+}
+
 pub fn interpret<R: BufRead, W: Write>(instructions: &Vec<Instruction>, state: &mut ExecutionState, in_stream: &mut R, out_stream: &mut W) -> () {
     while state.instruction_ptr < instructions.len() {
         let current_instruction = instructions[state.instruction_ptr];
 
-        match current_instruction {
-            Instruction::ApplyOperatorToCell { operator, value } => {
-                let actual_value = value.determine_value(state.get_current_cell());
-                let new_cell_value = apply_math_operator(state.get_current_cell(), operator, actual_value);
-                state.set_current_cell(new_cell_value);
-            }
-
-            Instruction::AddToCellPtr { direction, offset } => {
-                let abs_offset = offset.determine_value(state.get_current_cell());
-                let signed_offset = if direction == Direction::Left { abs_offset as isize * -1 } else { abs_offset as isize };
-                let new_cell_ptr = add_cell_ptr_value(state.cell_ptr, signed_offset);
-                state.set_cell_pointer(new_cell_ptr);
-            }
-
-            Instruction::JumpToIf { position, operator, match_value } => {
-                match operator {
-                    EqualityOperator::Equal => {
-                        if state.get_current_cell() == match_value {
-                            state.set_instruction_pointer(position);
-                        }
-                    },
-                    EqualityOperator::NotEqual => {
-                        if state.get_current_cell() != match_value {
-                            state.set_instruction_pointer(position);
-                        }
-                    }
-                }
-            }
-
-            Instruction::PrintOut => {
-                print_value(out_stream, state.get_current_cell());
-            }
-
-            Instruction::ReadIn => {
-                let input = read_value(in_stream);
-                state.set_current_cell(input);
-            }
-
-            Instruction::SetCell { value } => {
-                let actual_value = value.determine_value(state.get_current_cell());
-                state.set_current_cell(actual_value);
-            }
-        }
+        interpret_instruction(current_instruction, state, in_stream, out_stream);
 
         state.instruction_ptr += 1;
-        // println!("Cell Ptr: {cell_ptr}, Inst Ptr: {instruction_ptr} Cells: {cells:?}");
     }
 }
 
@@ -142,16 +145,168 @@ mod tests {
     use crate::ezfuck::parser::parser::compile_to_intermediate;
     use super::*;
 
-    fn interpret_and_collect_output(instructions: &Vec<Instruction>, input: &[u8]) -> String {
+    fn interpret_and_collect_output(instructions: &Vec<Instruction>, state: &mut ExecutionState, input: &[u8]) -> String {
         let mut input = &input[..];
         let mut output = vec![];
 
-        let mut state = ExecutionState::new();
-
-        interpret(&instructions, &mut state, &mut input, &mut output);
+        interpret(&instructions, state, &mut input, &mut output);
 
         let output_string = String::from_utf8(output).unwrap();
         return output_string;
+    }
+
+    fn interpret_instruction_and_collect_output(instruction: Instruction, state: &mut ExecutionState, input: &[u8]) -> String {
+        let mut input = &input[..];
+        let mut output = vec![];
+
+
+        interpret_instruction(instruction, state, &mut input, &mut output);
+
+        let output_string = String::from_utf8(output).unwrap();
+        return output_string;
+    }
+
+    #[test]
+    fn it_should_add_to_the_current_cell() {
+        let instruction = Instruction::ApplyOperatorToCell {
+            operator: MathOperator::Addition,
+            value: InstructionValue::Number(5),
+        };
+
+        let mut state = ExecutionState::new();
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.cells, vec![5]);
+    }
+
+    #[test]
+    fn it_should_subtract_from_the_current_cell() {
+        let instruction = Instruction::ApplyOperatorToCell {
+            operator: MathOperator::Subtraction,
+            value: InstructionValue::Number(5),
+        };
+
+        let mut state = ExecutionState::new();
+        state.set_current_cell(20);
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.cells, vec![15]);
+    }
+
+    #[test]
+    fn it_should_multiply_the_current_cell() {
+        let instruction = Instruction::ApplyOperatorToCell {
+            operator: MathOperator::Multiplication,
+            value: InstructionValue::Number(5),
+        };
+
+        let mut state = ExecutionState::new();
+        state.set_current_cell(10);
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.cells, vec![50]);
+    }
+
+    #[test]
+    fn it_should_divide_the_current_cell() {
+        let instruction = Instruction::ApplyOperatorToCell {
+            operator: MathOperator::Division,
+            value: InstructionValue::Number(5),
+        };
+
+        let mut state = ExecutionState::new();
+        state.set_current_cell(50);
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.cells, vec![10]);
+    }
+
+    #[test]
+    fn it_should_jump_the_instruction_pointer_if_equal() {
+        let instruction = Instruction::JumpToIf {
+            operator: EqualityOperator::Equal,
+            match_value: 10,
+            position: 5,
+        };
+
+        let mut state = ExecutionState::new();
+        state.set_current_cell(10);
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.instruction_ptr, 5);
+    }
+
+    #[test]
+    fn it_should_not_jump_the_instruction_pointer_if_not_equal() {
+        let instruction = Instruction::JumpToIf {
+            operator: EqualityOperator::Equal,
+            match_value: 100,
+            position: 5,
+        };
+
+        let mut state = ExecutionState::new();
+        state.set_current_cell(10);
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.instruction_ptr, 0);
+    }
+
+    #[test]
+    fn it_should_jump_the_instruction_pointer_if_not_equal() {
+        let instruction = Instruction::JumpToIf {
+            operator: EqualityOperator::NotEqual,
+            match_value: 10,
+            position: 5,
+        };
+
+        let mut state = ExecutionState::new();
+        state.set_current_cell(5);
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.instruction_ptr, 5);
+    }
+
+    #[test]
+    fn it_should_move_the_instruction_pointer_to_the_left() {
+        let instruction = Instruction::AddToCellPtr {
+            direction: Direction::Left,
+            offset: InstructionValue::Number(5),
+        };
+
+        let mut state = ExecutionState::new();
+        state.set_cell_pointer(20);
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.cell_ptr, 15);
+    }
+
+    #[test]
+    fn it_should_set_the_current_cell() {
+        let instruction = Instruction::SetCell {
+            value: InstructionValue::Number(5),
+        };
+
+        let mut state = ExecutionState::new();
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.cells, vec![5]);
+    }
+
+    #[test]
+    fn it_should_move_the_instruction_pointer_to_the_right() {
+        let instruction = Instruction::AddToCellPtr {
+            direction: Direction::Right,
+            offset: InstructionValue::Number(5),
+        };
+
+        let mut state = ExecutionState::new();
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.cell_ptr, 5);
+    }
+
+    #[test]
+    fn it_should_not_jump_the_instruction_pointer_if_equal() {
+        let instruction = Instruction::JumpToIf {
+            operator: EqualityOperator::NotEqual,
+            match_value: 10,
+            position: 5,
+        };
+
+        let mut state = ExecutionState::new();
+        state.set_current_cell(10);
+        interpret_instruction_and_collect_output(instruction, &mut state, b"");
+        assert_eq!(state.instruction_ptr, 0);
     }
 
     #[test]
@@ -160,7 +315,8 @@ mod tests {
         let code = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
         let instructions = compile_to_intermediate(code);
 
-        let output_string = interpret_and_collect_output(&instructions, b"");
+        let mut state = ExecutionState::new();
+        let output_string = interpret_and_collect_output(&instructions, &mut state, b"");
         assert_eq!(output_string, "Hello World!\n");
     }
 
@@ -169,7 +325,8 @@ mod tests {
         let code = "+8[>+4[>+2>+3>+3>+<4-]>+>+>->2+[<]<-]>2.>-3.+7..+3.>2.<-.<.+3.-6.-8.>2+.>+2.";
         let instructions = compile_to_intermediate(code);
 
-        let output_string = interpret_and_collect_output(&instructions, b"");
+        let mut state = ExecutionState::new();
+        let output_string = interpret_and_collect_output(&instructions, &mut state, b"");
         assert_eq!(output_string, "Hello World!\n");
     }
 
@@ -178,7 +335,8 @@ mod tests {
         let code = "^65 .";
         let instructions = compile_to_intermediate(code);
 
-        let output_string = interpret_and_collect_output(&instructions, b"");
+        let mut state = ExecutionState::new();
+        let output_string = interpret_and_collect_output(&instructions, &mut state, b"");
         assert_eq!(output_string, "A");
     }
 
@@ -196,7 +354,8 @@ mod tests {
         let code = "^^65 .";
         let instructions = compile_to_intermediate(code);
 
-        let output_string = interpret_and_collect_output(&instructions, b"");
+        let mut state = ExecutionState::new();
+        let output_string = interpret_and_collect_output(&instructions, &mut state, b"");
         assert_eq!(output_string, "A");
     }
 
